cbe65f40721e5abd2463ff6cad7eb521f3802763
Des Ryan <ryand25@tcd.ie>
2026-06-30 12:06:35 +0100

    under the hood added

---
+++ under_the_hood.R
@@ -0,0 +1,41 @@
+
+quarto preview speak_native_smurfit.qmd --to default --no-watch-inputs --no-browse
+
+quarto preview speak_native_smurfit.qmd --to pptx --no-watch-inputs --no-browse
+
+https://github.com/a2dez
+
+git branch 
+git remote add origin https://github.com/A2dez/speak_like_a_native_smurfit_202606.git
+git remote -v
+git push
+git push origin master
+git status
+git diff
+git remote -v
+git pull
+
+
+
+# -------------------------------------------------------------------------
+
+
+git branch --set-upstream-to=origin/master master
+git pull
+git push -u origin master
+
+
+
+git init
+git add .
+git commit -m "Initial commit"
+git remote add origin https://github.com/USERNAME/REPO.git
+git push -u origin master    # or main
+
+After that, your workflow is simply:
+  
+  git add .
+git commit -m "Describe your changes"
+git push
+
+And if you make changes on GitHub (or from another machine):
